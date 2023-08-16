aa18b6f74bee854082865e6284c7a1980d9f911c
Martin Vuong Nguyen <ngumartin@gmail.com>
2023-08-16 12:31:12 -0700

    Set mobile view, rearranged and resized image to fit for desktop view and mobile view.

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -4,13 +4,13 @@ import React, { Suspense, useEffect, useState } from "react";
 
 import CanvasLoader from '../Loader';
 
-const Computers = () => {
+const Computers = ({ isMobile }) => {
   const computer = useGLTF('./desktop_pc/scene.gltf')
 
   return (
     <mesh>
       <hemisphereLight intensity={1.15} groundColor='black' />
-      <pointLight intensity={20} />
+      <pointLight intensity={8} />
       <spotLight 
         position={[-20, 50, 10]}  
         angle={0.12} 
@@ -21,14 +21,33 @@ const Computers = () => {
 
       <primitive 
         object={computer.scene}
-        scale={0.65}
-        position={[ 0, -3.25, -1.5]}
+        scale={isMobile ? 0.25 : 0.55}
+        position={isMobile ? [0, -0.9, -0.5] : [ 0, -2.4, -0.8]}
         rotation={[ -0.01, -0.2, -0.1]}
       />
     </mesh>
   )
 }
 const ComputersCanvas = () => {
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    const mediaQuery = window.matchMedia('(max-width: 500px)');
+
+    setIsMobile(mediaQuery.matches);
+
+    const handleMediaQueryChange = (event) => {
+      setIsMobile(event.matches);
+    }
+
+    mediaQuery.addEventListener('change', handleMediaQueryChange);
+
+    return () => {
+      mediaQuery.removeEventListener('change', handleMediaQueryChange);
+    }
+
+  }, [])
+
   return (
     <Canvas 
       frameLoop='demand'
@@ -42,7 +61,7 @@ const ComputersCanvas = () => {
           maxPolarAngle={Math.PI / 2}
           minPolarAngle={Math.PI / 2}
         />
-        <Computers />
+        <Computers isMobile={isMobile} />
       </Suspense>
 
       <Preload all />
