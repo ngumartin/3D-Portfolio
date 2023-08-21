12f48a352acddb8623efb89f5b0ad7e0e89a269d
Martin Vuong Nguyen <ngumartin@gmail.com>
2023-08-21 11:35:23 -0700

    Added text and styled card

--- src/components/Feedbacks.jsx
+++ src/components/Feedbacks.jsx
@@ -5,6 +5,15 @@ import { SectionWrapper } from '../hoc';
 import { fadeIn, textVariant } from '../utils/motion';
 import { testimonials } from '../constants';
 
+const FeedbackCard = ({ index, testimonial, name, designation, company, image }) => (
+  <motion.div
+    variants={fadeIn('', 'spring', index * 0.5, 0.75)}
+    className='bg-black-200 p-10 rounded-3xl xs:w-[320px] w-full'
+  >
+    <p>{testimonial}</p>
+  </motion.div>
+)
+
 const Feedbacks = () => {
   return (
     <div className='mt-12 bg-black-100 rounded-[20px]'>
@@ -13,6 +22,15 @@ const Feedbacks = () => {
           <p className={styles.sectionSubText}>What others say</p>
           <h2 className={styles.sectionHeadText}>Testimonials.</h2>
         </motion.div>
+      </div>
+      <div className={`${styles.paddingX} -mt-20 pb-14 flex flex-wrap gap-7`}>
+        {testimonials.map((testimonial, index) => (
+          <FeedbackCard 
+            key={testimonial.name}
+            index={index}
+            {...testimonial}
+          />
+        ))}
       </div>
     </div>
   )
