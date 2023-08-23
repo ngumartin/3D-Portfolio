2bfc74c49fdb59e31d3f6f3b05e5d92a9d5e59cf
Martin Vuong Nguyen <ngumartin@gmail.com>
2023-08-23 10:14:07 -0700

    Created stars component and started import and setup for component.

--- src/components/canvas/Stars.jsx
+++ src/components/canvas/Stars.jsx
@@ -1,8 +1,19 @@
-import React from 'react'
+import { useState, useRef, Suspense } from 'react';
+import { Canvas, useFrame } from '@react-three/fiber';
+import { Points, PointMaterial, Preload } from '@react-three/drei';
+import * as random from 'maath/random/dist/maath-random.esm';
 
-const Stars = () => {
+const Stars = (props) => {
   return (
     <div>Stars</div>
+  )
+}
+
+const StarsCanvas = () => {
+  return (
+    <div className=''>
+
+    </div>
   )
 }
 
